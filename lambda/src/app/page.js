182d97b948e2b4aec3df6f70314bf610c9f0f163
morningstar <mandeep7yadav@gmail.com>
2024-06-22 21:08:52 +0530

    setupDone

--- lambda/src/app/page.js
+++ lambda/src/app/page.js
@@ -3,8 +3,13 @@ import React from 'react'
 const page = () => {
   return (
     <>
-    Hello World
+    <div className="medium tracking-[-1px] flex gap-1 text-xl items-center select-none text-white">
+      <span className="text-3xl light">[</span>
+      lambda
+      <span className="text-3xl light">]</span>
+    </div>
     </>
+    
   )
 }
 
