d39136c47e78372e52694c68eea86d2fd4bff008
morningstar <mandeep7yadav@gmail.com>
2024-06-24 21:43:11 +0530

    searchDone

--- lambda/src/shared/Notification.jsx
+++ lambda/src/shared/Notification.jsx
@@ -4,7 +4,7 @@ import { IoMdArrowDropright } from "react-icons/io";
 const Notification = () => {
   return (
     <>
-      <div className="w-full h-full medium   overflow-y-auto">
+      <div className="w-full h-full medium overflow-y-auto">
         <div className="flex items-center gap-1 lg:px-5 py-4 sticky top-0 bg-white shadow">
           <IoMdArrowDropright
             size={30}
@@ -14,7 +14,7 @@ const Notification = () => {
           <h1 className="lg:text-[2.2rem] sm:text-sm ">Notifications</h1>
         </div>
 
-        <div className="mt-4">
+        <div className="mt-1">
           <div className="flex medium gap-2 p-2 rounded-[5px] mx-2 cursor-pointer hover:bg-[#EEEEF8] border-b">
             <div className="w-14 h-12 overflow-hidden border rounded-[5px]">
               <img
@@ -41,8 +41,8 @@ const Notification = () => {
                   ...
                 </p>
                 <div className="text-white flex justify-center gap-5 items-center mr-3">
-                  <button className="bg-[#FF8D68] px-2 rounded">Accept</button>
-                  <button className="bg-[#7678ED] px-2 rounded">Reject</button>
+                  <button className="bg-[#FF8D68] px-2 rounded hover:bg-[#E16A44]">Accept</button>
+                  <button className="bg-[#7678ED] px-2 rounded hover:bg-[#6062C6]">Reject</button>
                 </div>
               </div>
             </div>
--- lambda/src/shared/Search.jsx
+++ lambda/src/shared/Search.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+import { Stack } from "@mui/material";
+import { sampleChats } from "@/components/sampleData";
 
 const Search = () => {
   return (
-    <div>Search</div>
-  )
-}
+    <>
+      <Stack width={"100%"} direction={"column"} className="h-full">
+        <div className="bg-white sticky top-0 z-10">
+          <div className="flex items-center gap-2 bg-[#DBDCFF] mx-2 my-3 px-3 py-2 rounded-[5px]">
+            <IoSearch color="gray" />
+            <input
+              type="text"
+              className="outline-none bg-transparent text-sm w-full light"
+              placeholder="Find Peoples"
+            />
+          </div>
+        </div>
+        <div className="h-full overflow-y-auto">
+        {sampleChats?.map((data, index) => {
+          return (
+              <div key={index} className="flex medium gap-2 p-2 rounded-[5px] mx-2 cursor-pointer hover:bg-[#EEEEF8] border-b">
+              <div className="w-14 h-12 overflow-hidden border rounded-[5px]">
+                <img
+                  src={data.avatar}
+                  className="object-cover w-full h-full align-middle"
+                  alt=""
+                />
+              </div>
+              <div className="flex flex-col w-full">
+                <div className="flex items-center justify-between">
+                  <p>{data.name}</p>
+                  <div className="flex items-center gap-2 px-2">
+                    <p className="text-xs text-gray-600">40m ago</p>
+                  </div>
+                </div>
+                <div className="flex justify-between items-center">
+                  <p className="text-sm text-gray-600">
+                    {"🌸 Dreamer, adventurer, and lover of all things cozy 🌸 | Living life one coffee at a time ☕ | Always chasing sunsets and good vibes ✨ | 📚 Bookworm | 🌿 Plant mama | 🐾 Dog lover | Smiling through the chaos, finding joy in the little things 💖 | Let's be friends! 💌".slice(
+                      0,
+                      50
+                    )}
+                    ...
+                  </p>
+                  <div className="text-white flex justify-center gap-5 items-center mr-3">
+                    <button className="bg-[#FF8D68] px-2 rounded hover:bg-[#E16A44]">Accept</button>
+                    <button className="bg-[#7678ED] px-2 rounded hover:bg-[#6062C6]">Reject</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+        </div>
+      </Stack>
+    </>
+  );
+};
 
-export default Search
+export default Search;
