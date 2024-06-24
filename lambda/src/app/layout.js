991f7c45f8d4d6d27e43706d108da664f4408557
morningstar <mandeep7yadav@gmail.com>
2024-06-24 22:08:04 +0530

    searchDone

--- lambda/src/app/layout.js
+++ lambda/src/app/layout.js
@@ -4,6 +4,9 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>lambda</title>
+      </head>
       <body>{children}</body>
     </html>
   );
