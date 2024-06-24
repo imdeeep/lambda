e4b82f093a9248afb7a34d6c4d64db8f717a85ec
morningstar <mandeep7yadav@gmail.com>
2024-06-25 01:48:53 +0530

    ProfileDone

--- lambda/src/shared/Profile.jsx
+++ lambda/src/shared/Profile.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { IoPersonAddSharp } from "react-icons/io5";
 import { IoMdSettings } from "react-icons/io";
+import { IoMdSave } from "react-icons/io";
 import { MdOutlineFileUpload } from "react-icons/md";
 
 const Profile = () => {
@@ -21,9 +22,9 @@ const Profile = () => {
     "🌸 Dreamer, adventurer, and lover of all things cozy 🌸 | Living life one coffee at a time ☕ | Always chasing sunsets and good vibes ✨ | 📚 Bookworm | 🌿 Plant mama | 🐾 Dog lover | Smiling through the chaos, finding joy in the little things 💖 | Let's be friends! 💌";
 
   return (
-    <div className="w-full h-full medium width100vw">
+    <div className="w-full h-full medium width100vw mb-[8rem] md:mb-[0rem]">
       {user ? (
-        <div className="flex gap-10 items-center mt-5 ml-10">
+        <div className="flex flex-col gap-10 items-center md:flex-row pt-8 md:ml-10 ">
           <div className="w-[13rem] h-[13rem] overflow-hidden border rounded-full border-[3px] border-[#7678ED] p-1">
             <img
               src="https://img.freepik.com/free-photo/young-female-model-portrait_23-2149084889.jpg"
@@ -35,12 +36,12 @@ const Profile = () => {
             <div className="flex gap-10">
               <h1 className="text-2xl tracking-[-1px]">{"JasminLowrey69"}</h1>
               {friend ? (
-                <div className="flex gap-2">
+                <div className="flex flex-col md:flex-row gap-2">
                   <button className="bg-[#FF8D68] px-2 rounded hover:bg-[#E16A44] text-white">
                     Message
                   </button>
                   <button
-                    className="bg-[#7678ED] px-2 rounded hover:bg-[#6062C6] text-white"
+                    className="bg-[#7678ED] px-2 rounded hover:bg-[#6062C6] text-white "
                     onClick={handleRemoveFriend}
                   >
                     Remove Friend
@@ -63,7 +64,8 @@ const Profile = () => {
           </div>
         </div>
       ) : (
-        <div className="flex gap-10 items-center mt-5 ml-10">
+        // User is not logged in
+        <div className="flex flex-col gap-10 items-center md:flex-row pt-8 md:ml-10 ">
           <div className="flex flex-col items-center gap-1">
             <div className="w-[13rem] h-[13rem] overflow-hidden border rounded-full border-[3px] border-[#7678ED] p-1">
               <img
@@ -74,7 +76,7 @@ const Profile = () => {
             </div>
             {edit ? (
               <>
-                <button className="bg-[#7678ED] text-white rounded-full px-1 flex items-center gap-1">
+                <button className="bg-[#7678ED] text-white text-sm md:text-l rounded-full px-1 flex items-center gap-1">
                   <MdOutlineFileUpload />
                   Change photo{" "}
                 </button>
@@ -84,9 +86,9 @@ const Profile = () => {
             )}
           </div>
           <div className="w-[50%]">
-            <div className="flex gap-10">
+            <div className="flex gap-10 items-center">
               <h1 className="text-2xl tracking-[-1px]">{"JasminLowrey69"}</h1>
-              <div className="flex gap-2">
+              <div className="flex flex-col md:flex-row gap-2">
                 <button
                   className="bg-[#FF8D68] px-2 rounded hover:bg-[#E16A44] text-white flex items-center gap-1"
                   onClick={handleEdit}
@@ -96,9 +98,10 @@ const Profile = () => {
                 </button>
                 {edit ? (
                   <button
-                    className="bg-[#7678ED] px-2 rounded hover:bg-[#6062C6] text-white"
+                    className="bg-[#7678ED] px-2 rounded hover:bg-[#6062C6] text-white flex items-center gap-1"
                     onClick={handleSave}
                   >
+                    <IoMdSave />
                     Save
                   </button>
                 ) : (
@@ -111,7 +114,7 @@ const Profile = () => {
                 <input
                   type="text"
                   value={"Jasmin Lowrey"}
-                  className="outline-none border rounded px-1 inline-block text-xl light mt-3"
+                  className="outline-none border rounded px-1 inline-block text-xl light text-start w-full mt-3"
                 />
                 <br />
               </>
